96fef60f54715b3de159d22d235d8630090aa9bd
Louis Allard <la.deweb@yahoo.com>
2025-05-20 13:15:44 +0200

    Fix.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <Calc />
+      <br />
+      <i><small><a href="http:\\www.louisallard.fr" target="_blank" rel="noreferrer">www.louisallard.fr</a></small></i>
     </div>
   );
 }
--- src/components/Calc.js
+++ src/components/Calc.js
@@ -1,61 +1,80 @@
 import React, { useState } from 'react';
+import Tab from './Tab';
 
-function CalculInteretCompose() {
-  const [valeurInitiale, setValeurInitiale] = useState(2500);
-  const [tauxAugmentation, setTauxAugmentation] = useState(3);
-  const [periode, setPeriode] = useState(10);
-  const [valeurFinale, setValeurFinale] = useState(0);
+const Calc = () => {
+  const [formData, setFormData] = useState({
+    valeurInitiale: '',
+    taux: '',
+    periode: '',
+  });
+  const [results, setResults] = useState(null);
 
-  const calculerValeurFinale = () => {
-    const r = tauxAugmentation / 100;
-    const n = periode;
-    const V_i = valeurInitiale;
-    const V_f = V_i * Math.pow(1 + r, n);
-    setValeurFinale(V_f);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setResults(formData);
   };
 
   return (
     <div>
-      <h2>Calcul de l'intérêt composé</h2>
-      <div>
-        <label>
-          Valeur initiale :
-          </label>
-          <input
-            type="number"
-            value={valeurInitiale}
-            onChange={(e) => setValeurInitiale(parseFloat(e.target.value))}
-          />
-      </div>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Calcul d'intérêts composés</legend>
+          <div>
+            <label>
+              Valeur Initiale:
+            </label>
+            <input
+              type="number"
+              name="valeurInitiale"
+              value={formData.valeurInitiale}
+              onChange={handleChange}
+            />
+          </div>
 
-      <div>
-        <label>
-          Taux d'augmentation (%) :
-          </label>
-          <input
-            type="number"
-            value={tauxAugmentation}
-            onChange={(e) => setTauxAugmentation(parseFloat(e.target.value))}
-          />
-      </div>
+          <div>
+            <label>
+              Taux:
+            </label>
+            <input
+              type="number"
+              name="taux"
+              value={formData.taux}
+              onChange={handleChange}
+            />
+          </div>
 
-      <div>
-        <label>
-          Période (années) :
-          </label>
-          <input
-            type="number"
-            value={periode}
-            onChange={(e) => setPeriode(parseInt(e.target.value))}
-          />
-      </div>
-      
-      <button onClick={calculerValeurFinale}>Calculer</button>
-      <div>
-        <p>Valeur finale : {valeurFinale.toFixed(2)}</p>
-      </div>
+          <div>
+            <label>
+              Période (année):
+            </label>
+            <input
+              type="number"
+              name="periode"
+              value={formData.periode}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Soumettre</button>
+        </fieldset>
+      </form>
+
+      {results && (
+        <Tab
+          valeurInitiale={results.valeurInitiale}
+          taux={results.taux}
+          periode={results.periode}
+        />
+      )}
     </div>
   );
-}
+};
 
-export default CalculInteretCompose;
+export default Calc;
